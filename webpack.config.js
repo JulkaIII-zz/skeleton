732c1de2d84252fc76bd33221581d78965af14e7
Yuliia Shtohryn <julia.ilkiv@gmail.com>
2018-09-04 17:55:48 +0300

    Finished Output Management section

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // generate its own index.html file, replaces our.
+const CleanWebpackPlugin = require('clean-webpack-plugin');// cleans a dist folder before build
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   }//,
   // module: {
