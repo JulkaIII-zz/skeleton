4005e6ac0757e8e275c6b683b226cea16d431eff
Yuliia Shtohryn <julia.ilkiv@gmail.com>
2018-09-04 16:57:17 +0300

    Finished Asset management section

--- webpack.config.js
+++ webpack.config.js
@@ -3,46 +3,46 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      //loading scc
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      //loading images
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      //loading fonts
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          'file-loader'
-        ]
-      },
-      //loading csv, xml; json-by default
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-          'csv-loader'
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use: [
-          'xml-loader'
-        ]
-      }
-    ]
-  }
+  }//,
+  // module: {
+  //   rules: [
+  //     //loading scc
+  //     {
+  //       test: /\.css$/,
+  //       use: [
+  //         'style-loader',
+  //         'css-loader'
+  //       ]
+  //     },
+  //     //loading images
+  //     {
+  //       test: /\.(png|svg|jpg|gif)$/,
+  //       use: [
+  //         'file-loader'
+  //       ]
+  //     },
+  //     //loading fonts
+  //     {
+  //       test: /\.(woff|woff2|eot|ttf|otf)$/,
+  //       use: [
+  //         'file-loader'
+  //       ]
+  //     },
+  //     //loading csv, xml; json-by default
+  //     {
+  //       test: /\.(csv|tsv)$/,
+  //       use: [
+  //         'csv-loader'
+  //       ]
+  //     },
+  //     {
+  //       test: /\.xml$/,
+  //       use: [
+  //         'xml-loader'
+  //       ]
+  //     }
+  //   ]
+  // }
 };
